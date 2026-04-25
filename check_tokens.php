c6ada89807507b8f5b6249033eb3e5864e938525
FaakhirMemon030 <alexwatson031105@gmail.com>
2026-04-25 15:56:08 +0500

    chore: remove unused check_tokens debugging script

--- check_tokens.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'includes/db.php';
-$stmt = $pdo->query("SELECT email, reset_token, reset_token_expiry FROM users");
-$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo "Users:\n";
-print_r($users);
